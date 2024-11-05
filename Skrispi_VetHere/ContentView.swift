af43d58f2245b1cd7b865f230c426eda5047cf84
Christian Gunawan <cg404cgunawan@gmail.com>
2024-11-05 10:43:00 +0700

    Initial Commit

---
+++ Skrispi_VetHere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skrispi_VetHere
+//
+//  Created by Christian Gunawan on 05/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skrispi_VetHere/Skrispi_VetHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skrispi_VetHereApp.swift
+//  Skrispi_VetHere
+//
+//  Created by Christian Gunawan on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Skrispi_VetHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
